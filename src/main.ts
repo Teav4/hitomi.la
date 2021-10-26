f5fe31ebba4225dbaf5923145c12e56690cc38c6
Teav4 <teav4@feadorra.com>
2021-10-26 12:49:00 +0700

    mongo authen

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import databaseConfig from './config/database.config'
 import crawler from './crawler'
 
-const DB_CONNECTION_URL = `${databaseConfig.MONGO_HOSTNAME}:${databaseConfig.MONGO_PORT}/${databaseConfig.MONGO_DB}`
+const DB_CONNECTION_URL = `mongodb://${databaseConfig.MONGO_USERNAME}:${databaseConfig.MONGO_PASSWORD}@${databaseConfig.MONGO_HOSTNAME}:${databaseConfig.MONGO_PORT}/${databaseConfig.MONGO_DB}`
 
 mongoose
 	.connect(DB_CONNECTION_URL)
