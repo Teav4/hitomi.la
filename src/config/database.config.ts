1517ebb3a5392811fe37fda40279b89db90b8ab5
Teav4 <teav4@feadorra.com>
2021-10-26 12:52:50 +0700

    fix Password contains unescaped characters

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,6 +6,6 @@ export default {
   MONGO_HOSTNAME: process.env.MONGO_HOSTNAME,
   MONGO_PORT: process.env.MONGO_PORT,
   MONGO_DB: process.env.MONGO_DB,
-  MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+  MONGO_PASSWORD: encodeURIComponent(process.env.MONGO_PASSWORD + ''),
   MONGO_USERNAME: process.env.MONGO_USERNAME
 }
